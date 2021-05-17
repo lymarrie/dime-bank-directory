0740321c5af230ae215c3d4b5df2e6de3377d523
gweiler <gweiler@yext.com>
2021-05-17 16:04:37 -0400

    initial setup of tailwind

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+
+      }, 
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //       template: "./src/template.html",
+    //     }),
+    // ],
+    output: {
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, 'public'),
+        clean: true
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+            // Order is last to first
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1
+                    }
+                }, 
+                'postcss-loader'
+            ],
+          },
+        ],
+      }
+
+};
